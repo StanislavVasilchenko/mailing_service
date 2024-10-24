461228a9fafc1c0d12aa91a99a1eb675094d3a09
stanislav <stanislav.vasilchenko@yandex.ru>
2024-10-24 23:13:36 +0300

    create repo NotifyRepository

---
+++ src/notification/repository.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from .models import Notify
+from .schemas import NotifyCreate
+
+
+class NotifyRepository:
+    def __init__(self, session: Session):
+        self.model = Notify
+        self.db = session
+
+    def save(self, notify: Notify):
+        self.db.add(notify)
+        self.db.commit()
+        self.db.refresh(notify)
+
+    def create(self, notify: NotifyCreate):
+        notify = self.model(**notify.dict())
+        self.save(notify)
+        return notify
