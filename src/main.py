52ea4515c4a507a6a06ceace31b403a31e1b2f20
stanislav <stanislav.vasilchenko@yandex.ru>
2024-10-26 01:03:04 +0300

    application launch changed

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from src.notification.router import router as notify_router
+import uvicorn
+
 
 app = FastAPI()
 app.include_router(notify_router)
 
 
-@app.get("/")
-async def hello():
-    return {"message": "hello_world"}
+if __name__ == "__main__":
+    uvicorn.run("src.main:app", reload=True)
