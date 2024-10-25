18588083c9c1348396f40185bccb0ba029fc5d0a
stanislav <stanislav.vasilchenko@yandex.ru>
2024-10-25 21:27:27 +0300

    add redis settings and email settings

--- private_keys.py
+++ private_keys.py
@@ -8,3 +8,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_PORT = os.getenv("DB_PORT")
+
+BROKER_REDIS_URL = os.getenv("BROKER_REDIS_URL")
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
