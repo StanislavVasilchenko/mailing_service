2aa18dc8c79a67fbdd08fb57e48a4de689b84944
stanislav <stanislav.vasilchenko@yandex.ru>
2024-10-26 01:01:06 +0300

    add send_to_telegram

--- src/notification/utils.py
+++ src/notification/utils.py
@@ -1,7 +1,10 @@
 import smtplib
+import requests
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from private_keys import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST, EMAIL_PORT
+from .constants import BASE_URL_TELEGRAM
+from private_keys import BOT_TOKEN
 
 
 def send_mail(receiver_email, message_text):
@@ -13,9 +16,14 @@ def send_mail(receiver_email, message_text):
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
-    message["Subject"] = "Тема письма"
+    message["Subject"] = message_text[:30]
     message.attach(MIMEText(message_text, "plain"))
 
-    server = smtplib.SMTP_SSL(smtp_server, smtp_port)
-    server.login(sender_email, password)
-    server.send_message(message)
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(sender_email, password)
+        server.send_message(msg=message)
+
+
+def send_to_telegram(telegram_id, message_text):
+    params = {"chat_id": telegram_id, "text": message_text}
+    requests.post(url=f"{BASE_URL_TELEGRAM}{BOT_TOKEN}/sendMessage", params=params)
--- src/tasks/task_notify.py
+++ src/tasks/task_notify.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from src.tasks.settings import celery_app
-from src.notification.utils import send_mail
+from src.notification.utils import send_mail, send_to_telegram
 from src.notification.repository import NotifyRepository
 from src.database import get_db
 
@@ -15,9 +15,8 @@ def send_notification(notify_id: int, session: Session = get_db().__next__()):
         if isinstance(notify_db.recipient, list)
         else [notify_db.recipient]
     )
-
     for address in recipient:
         if address.isdigit():
-            pass
+            send_to_telegram(telegram_id=address, message_text=notify_db.message)
         else:
             send_mail(receiver_email=address, message_text=notify_db.message)
