8e0883490680952715c07f2d3bfafb82ff873c55
stanislav <stanislav.vasilchenko@yandex.ru>
2024-10-26 12:30:34 +0300

    create model Logs

---
+++ src/log/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from src.database import Base
+
+
+class Logs(Base):
+    __tablename__ = "logs"
+
+    logs_id = Column(Integer, primary_key=True, autoincrement=True)
+    sent_at = Column(DateTime, default=datetime.now(), nullable=False)
+    email = Column(String(length=50), nullable=True)
+    telegram = Column(String(length=50), nullable=True)
