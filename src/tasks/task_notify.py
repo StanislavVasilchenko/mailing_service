9d3ad3d2bf7e59eb1eecfa568e1b83691d606cb5
stanislav <stanislav.vasilchenko@yandex.ru>
2024-10-26 13:19:34 +0300

    added sending logging

--- src/tasks/task_notify.py
+++ src/tasks/task_notify.py
@@ -3,6 +3,8 @@ from src.tasks.settings import celery_app
 from src.notification.utils import send_mail, send_to_telegram
 from src.notification.repository import NotifyRepository
 from src.database import get_db
+from src.log.repository import LogsRepositories
+from src.log.models import Logs
 
 
 @celery_app.task
@@ -16,7 +18,13 @@ def send_notification(notify_id: int, session: Session = get_db().__next__()):
         else [notify_db.recipient]
     )
     for address in recipient:
+        repo_logs = LogsRepositories(session)
         if address.isdigit():
             send_to_telegram(telegram_id=address, message_text=notify_db.message)
+            log = Logs(address=address)
         elif "@" in address:
             send_mail(receiver_email=address, message_text=notify_db.message)
+            log = Logs(address=address)
+        else:
+            log = Logs(address=address, sent=False)
+        repo_logs.save(log)
